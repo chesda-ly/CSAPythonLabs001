e28841ff5385d44e20506add8be193df0d9be5cd
Sopheakchesda_Ly <2023448ly@aupp.edu.kh>
2024-01-28 16:54:10 +0700

    ENTER

--- process-string.py
+++ process-string.py
@@ -6,6 +6,11 @@
 # * "Hello World" -> "HHeelllloo  WWoorrlldd"
 # * "1234!_ "     -> "11223344!!__  "
 
+word = input("Enter a word: ")
+
+result_1 = '' .join(char * 2 for char in word)
+
+print(result_1)
 
 # Application 2
 #  Given a string indicating a range of letters, return a string which includes all the letters in that range, including the last letter. Note that if the range is given in capital letters, return the string in capitals also!
@@ -17,8 +22,12 @@
 # "J-J" ➞ "J"
 # Notes A hyphen will separate the two letters in the string.
 
+input_2 = input("Enter a range of letters (e.g., a-z): ")
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-start, end = alphabet.split('-')
-user_range = input("Enter a range of letters (e.g., a-z): ")
 
+start, end = input_2.split('-')
+
+result_2 = '' .join(chr(i) for i in range(ord(start), ord(end) + 1))
+
+print(result_2)
